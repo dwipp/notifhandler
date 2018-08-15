dffcf5c62bc31229e05f6cd8c1609bdeea4e8b59
Dwi Permana Putra <dwi.putra@icloud.com>
2018-08-15 21:59:36 +0800

    Init

---
+++ notifhandler/notifhandler/Sample.swift
@@ -0,0 +1,16 @@
+//
+//  Sample.swift
+//  notifhandler
+//
+//  Created by Dwi Permana Putra on 15/08/18.
+//  Copyright © 2018 Ropen Indonesia. All rights reserved.
+//
+
+import Foundation
+
+public class Sample {
+    private init(){}
+    public static func doSomething()->String {
+        return "it works well!"
+    }
+}
---
+++ notifhandler/notifhandler/notifhandler.h
@@ -0,0 +1,19 @@
+//
+//  notifhandler.h
+//  notifhandler
+//
+//  Created by Dwi Permana Putra on 15/08/18.
+//  Copyright © 2018 Ropen Indonesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for notifhandler.
+FOUNDATION_EXPORT double notifhandlerVersionNumber;
+
+//! Project version string for notifhandler.
+FOUNDATION_EXPORT const unsigned char notifhandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <notifhandler/PublicHeader.h>
+
+
