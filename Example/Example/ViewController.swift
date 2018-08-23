dfbf25e9666bf8d70b0d40b3e6f2b2986e646bdf
Dwi Permana Putra <dwi.putra@icloud.com>
2018-08-23 12:51:46 +0700

    update

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         SwipeDK.configure()
-//        SwipeDK.
         
         
         // Do any additional setup after loading the view, typically from a nib.
